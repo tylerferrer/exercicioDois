e2b54f7726cc2d5b508f6404785ba4ced10e5507
tylerferrer <tylerlucasferrer@gmail.com>
2022-10-03 17:53:55 -0300

    commit com projeto

---
+++ src/main/java/com/mycompany/exercicio2/Exercicio2.java
@@ -0,0 +1,20 @@
+package com.mycompany.exercicio2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Tyler
+ */
+public class Exercicio2 {
+
+    public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+                System.out.println("Digite um número: ");
+		int numero = entrada.nextInt();
+		
+		System.out.print("O número digitado foi: ");
+		System.out.print(numero);
+	}
+}
